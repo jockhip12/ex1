4dde7cf4579bc5714327d274dce42d1ccdea16f9
anouar <dsi.anouar.abid@gmail.com>
2018-03-28 16:59:48 +0200

    add routers and fix navbar menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProductService } from './services/product.service';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
+
+const routes = [
+   {path: 'welcome', component: WelcomeComponent},
+   {path: 'list', component: ProductsListComponent},
+   {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ProductsListComponent,
-    FooterComponent
+    FooterComponent,
+    WelcomeComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
